2a268b5a2c8821858a34e3e344510eeb6ce4b36a
ChezCoder <62308406+ChezCoder@users.noreply.github.com>
2021-05-01 20:04:55 -0400

    grasswalker bug fix

--- src/main/java/puffel_/moose/mod/mixin/GrassWalkerEnchantMixin.java
+++ src/main/java/puffel_/moose/mod/mixin/GrassWalkerEnchantMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.block.Blocks;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,7 +24,7 @@ public class GrassWalkerEnchantMixin {
 
 	private void tick(CallbackInfo info) {
 	    ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
-        BlockState standingOnBlockState = (BlockState) (Object) player.getEntityWorld().getBlockState((BlockPos) (Object) player.getPos());
+        BlockState standingOnBlockState = (BlockState) (Object) player.getEntityWorld().getBlockState(player.getBlockPos());
         Block standingOnBlock = standingOnBlockState.getBlock();
 
         if(standingOnBlock == Blocks.GRASS_BLOCK || standingOnBlock == ModBlocks.MOOSE_GRASS) {
